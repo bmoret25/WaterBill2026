0d66a245b8a87749e60b5d63d127a2d324614e26
bmoret25 <bmoret@western.edu>
2026-09-16 14:51:54 -0600

    Added Unit Tests and got rid of hard coded values in Main

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Customer MalloryCustomer = new Customer();
+        Customer MalloryCustomer = new Customer();  //create an instance of ccustomer
         System.out.println(MalloryCustomer.name);
         MalloryCustomer.name = "Mallory";
         System.out.println(MalloryCustomer.name);   //. allows me to access properties
+        MalloryCustomer.calculateBill();            //also have access to all methods bc public
 
         Customer dumbCustomer = new Customer();
         System.out.println(dumbCustomer.gallonsUsed);   //classes have their own default values, I don't need to initialize them
